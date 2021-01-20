ba44620be94386b6f06aa990579903ee8d368c61
Harrison Quadir <harrison@harrisons-mbp.home>
2021-01-20 22:59:55 +0000

    added prettier config

--- js/header.js
+++ js/header.js
@@ -10,21 +10,25 @@ const navSocials = document.querySelectorAll('.mobile-navbar__socials');
 
 // Toggling the hamburger menu
 function toggle() {
-    navBtn.classList.toggle('nav-open');
-    navOpen.classList.toggle('nav-open');
-    contentPushDown.classList.toggle('content-push-down-big');
+	navBtn.classList.toggle('nav-open');
+	navOpen.classList.toggle('nav-open');
+	contentPushDown.classList.toggle('content-push-down-big');
 }
 
 // Open and close the hamburger menu
 navBtn.addEventListener('click', () => {
-    toggle();
+	toggle();
 });
 
 // Close menu on click
-navLinks.forEach(navLink => navLink.addEventListener('click', e => {
-    toggle();
-}));
+navLinks.forEach(navLink =>
+	navLink.addEventListener('click', e => {
+		toggle();
+	})
+);
 
-navSocials.forEach(navSocial => navSocial.addEventListener('click', e => {
-    toggle();
-}));
+navSocials.forEach(navSocial =>
+	navSocial.addEventListener('click', e => {
+		toggle();
+	})
+);
